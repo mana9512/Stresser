3f05cfb97965bfd64ce9c92d829b375276dafac1
mana9512 <manashreepatel28@gmail.com>
2020-10-18 17:02:52 +0530

    personality radio buttons added

--- Stresser-Website/frontend/src/components/layout/Personality.js
+++ Stresser-Website/frontend/src/components/layout/Personality.js
@@ -90,7 +90,69 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
   const handleChange = (event, value) => {
     setPage(value);
   };
-
+  const [selected, setSelected] = React.useState();
+  const [questions,setQuestions] = useState([
+    [
+      ["I am the life of the party.", "EXT", 1,undefined],
+      // ["I don't talk a lot.", "EXT", 1,undefined],
+      // ["I feel comfortable around people.","EXT", 1,undefined],
+      // ["I keep in the background.","EXT", 1,undefined],
+      // ["I start conversations.","EXT", 1,undefined],
+      // ["I have little to say.","EXT", 1,undefined],
+      // ["I talk to a lot of different people at parties.","EXT", 1,undefined],
+      // ["I don't like to draw attention to myself.","EXT", 1,undefined],
+      // ["I don't mind being the center of attention.","EXT", 1,undefined],
+      // ["I am quiet around strangers.","EXT", 1,undefined],
+    ],
+    [
+      ["I get stressed out easily.", "EST", 1,undefined],
+      // ["I am relaxed most of the time.", "EST", 1,undefined],
+      // ["I worry about things.", "EST", 1,undefined],
+      // ["I seldom feel blue.", "EST", 1,undefined],
+      // ["I am easily disturbed.", "EST", 1,undefined],
+      // ["I get upset easily.", "EST", 1,undefined],
+      // ["I change my mood a lot.", "EST", 1,undefined],
+      // ["I have frequent mood swings.", "EST", 1,undefined],
+      // ["I get irritated easily.", "EST", 1,undefined],
+      // ["I often feel blue.", "EST", 1,undefined],
+    ],
+    [
+      ["I feel little concern for others.", "AGR", ,undefined],
+      // ["I am interested in people.", "AGR", 1,undefined],
+      // ["I insult people.", "AGR", 1,undefined],
+      // ["I sympathize with others' feelings.", "AGR", 1,undefined],
+      // ["I am not interested in other people's problems.", "AGR", 1,undefined],
+      // ["I have a soft heart.", "AGR", 1,undefined],
+      // ["I am not really interested in others.", "AGR", 1,undefined],
+      // ["I take time out for others.", "AGR", 1,undefined],
+      // ["I feel others' emotions.", "AGR", 1,undefined],
+      // ["I make people feel at ease.", "AGR", 1,undefined]
+    ],
+    [
+      ["I am always prepared.", "CSN", 1,undefined],
+      // ["I leave my belongings around.", "CSN", 1,undefined],
+      // ["I pay attention to details.", "CSN", 1,undefined],
+      // ["I make a mess of things.", "CSN", 1,undefined],
+      // ["I get chores done right away.", "CSN", 1,undefined],
+      // ["I often forget to put things back in their proper place.", "CSN", 1,undefined],
+      // ["I like order.", "CSN", 1,undefined],
+      // ["I shirk my duties.", "CSN", 1,undefined],
+      // ["I follow a schedule.", "CSN", 1,undefined],
+      // ["I am exacting in my work.", "CSN", 1,undefined],
+    ],
+    [
+      ["I have a rich vocabulary.", "OPN", 1,undefined],
+      // ["I have difficulty understanding abstract ideas.", "OPN", 1,undefined],
+      // ["I have a vivid imagination.", "OPN", 1,undefined],
+      // ["I am not interested in abstract ideas.", "OPN", 1,undefined],
+      // ["I have excellent ideas.", "OPN", 1,undefined],
+      // ["I do not have a good imagination.", "OPN", 1,undefined],
+      // ["I am quick to understand things.", "OPN", 1,undefined],
+      // ["I use difficult words.", "OPN", 1,undefined],
+      // ["I spend time reflecting on things.", "OPN", 1,undefined],
+      // ["I am full of ideas.", "OPN", 1,undefined],
+    ],
+  ])
   // const [userScores, setUserScores] = useState({
   //   EXT: 0,
   //   EST: 0,
@@ -114,73 +176,15 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
   const onChange = (page, index, value, e) => {
     e.preventDefault();
     let i = 0;
-
-    // console.log(questions[page][index][1]);
-    // console.log(questions[page][index][2]);
-    // console.log(value);
-    questions[page][index][2] = value;
-    if (questions[page][index][1] === "EXT") {
-      userScores["EXT"] = 0;
-
-      for (i = 0; i < questions[page].length; i++) {
-        if (i % 2 !== 0) {
-          userScores["EXT"] -= questions[page][i][2];
-        } else {
-          userScores["EXT"] += questions[page][i][2];
-        }
-      }
-      userScores["EXT"] += 20;
-      //   ext=(userScores["EXT"]*(8 -1))/ 40 + 1;
-    } else if (questions[page][index][1] === "OPN") {
-      userScores["OPN"] = 0;
-      for (i = 0; i < questions[page].length; i++) {
-        if (i % 2 !== 0) {
-          userScores["OPN"] -= questions[page][i][2];
-        } else {
-          userScores["OPN"] += questions[page][i][2];
-        }
-      }
-      userScores["OPN"] += 8;
-    } else if (questions[page][index][1] === "CSN") {
-      for (i = 0; i < questions[page].length; i++) {
-        userScores["CSN"] = 0;
-        if (i % 2 !== 0) {
-          userScores["CSN"] -= questions[page][i][2];
-        } else {
-          userScores["CSN"] += questions[page][i][2];
-        }
-      }
-      userScores["CSN"] += 14;
-    } else if (questions[page][index][1] === "AGR") {
-      userScores["AGR"] = 0;
-      for (i = 0; i < questions[page].length; i++) {
-        if (i % 2 !== 0) {
-          userScores["AGR"] += questions[page][i][2];
-        } else {
-          userScores["AGR"] -= questions[page][i][2];
-        }
-      }
-      userScores["AGR"] += 14;
-    } else if (questions[page][index][1] === "EST") {
-      userScores["EST"] = 0;
-      for (i = 0; i < questions[page].length; i++) {
-        if (i % 2 !== 0) {
-          userScores["EST"] += questions[page][i][2];
-        } else {
-          userScores["EST"] -= questions[page][i][2];
-        }
-      }
-      userScores["EST"] += 38;
+    for (i = 0; i < questions[page].length; i++) {
+      questions[page][i][3]=value
+      setQuestions(questions);
+      setSelected(value);
     }
-
+    
     console.log(userScores);
   };
-  // let ext=(userScores["EXT"]*(8 -1))/ 40 + 1;
-  // let opn=(userScores["OPN"]*(8 -1))/ 40 + 1;
-  // let agr=(userScores["AGR"]*(8 -1))/ 40 + 1;
-  // let csn=(userScores["CSN"]*(8 -1))/ 40 + 1;
-  // let est=(userScores["EST"]*(8 -1))/ 40 + 1;
-  // console.log(ext);
+  
 
   const Submit = (e) => {
     // if(!isAuthenticated){
@@ -229,8 +233,9 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
                     type="radio"
                     name={"option" + i}
                     id={i+"_"+page + "_0"}
-                    value="1"
                     
+                    value={1} 
+                    checked={questions[page - 1][i][3] == 1}
                     onChange={(e) => onChange(page - 1, i, 1, e)}
                   />
                   <label htmlFor={i +"_"+page + "_0"}>DISAGREE</label>
@@ -243,7 +248,9 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
                     type="radio"
                     name={"option" + i}
                     id={i +"_"+page +"_1"}
-                    value="1"
+                 
+                    value={2} 
+                    checked={questions[page - 1][i][3] == 2}
                     onChange={(e) => onChange(page - 1, i, 2, e)}
                   />
                   <label htmlFor={i +"_"+page + "_1"}>SLIGHTLY DISAGREE</label>
@@ -256,7 +263,8 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
                     type="radio"
                     name={"option" + i}
                     id={i +"_"+page + "_2"}
-                    value="3"
+                    value={3} 
+                    checked={questions[page - 1][i][3] == 3}
                     onChange={(e) => onChange(page - 1, i, 3, e)}
                   />
                   <label htmlFor={i +"_"+ page +"_2"}>NEUTRAL</label>
@@ -269,7 +277,8 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
                     type="radio"
                     name={"option" + i}
                     id={i +"_"+ page +"_3"}
-                    value="1"
+                    value={4} 
+                    checked={questions[page - 1][i][3] ==4 }
                     onChange={(e) => onChange(page - 1, i, 4, e)}
                   />
                   <label htmlFor={i +"_"+page + "_3"}>SLIGHTLY AGREE</label>
@@ -282,7 +291,8 @@ const Personality = ({ personalityscore, isAuthenticated, personalitypred, user 
                     type="radio"
                     name={"option" + i}
                     id={i +"_"+page + "_4"}
-                    value="5"
+                    value={5} 
+                    checked={questions[page - 1][i][3] == 5}
                     onChange={(e) => onChange(page - 1, i, 5, e)}
                   />
                   <label htmlFor={i +"_"+page + "_4"}>AGREE</label>
